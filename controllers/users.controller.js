af0d6ae7b6983d399a23a8a7d525f91e485df21f
SunnyDev7 <sunnydev4589@gmail.com>
2026-01-26 21:42:01 +0100

    count active users aggregator ready

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,9 +1,29 @@
+import { User } from "../models/index.js";
 import { findUsers } from "../services/users.service.js";
 
 export const getAllUsers = async (req, res) => {
   try {
     const users = await findUsers();
     res.json({ success: true, count: users.length, data: users });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export const countActiveUsers = async (req, res) => {
+  try {
+    const result = await User.aggregate([
+      {
+        $match: {
+          isActive: true,
+        },
+      },
+      {
+        $count: "activeUsers",
+      },
+    ]);
+
+    res.json({ success: true, data: result });
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
   }
